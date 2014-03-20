4d34815e1f93ee9185b7bebb82564bba0e77a44e
travisb22 <travis.britt@ymail.com>
2014-03-20 16:57:26 -0400

    lab 8-3

--- Britt_Travis_Lab8.js
+++ Britt_Travis_Lab8.js
@@ -1,5 +1,6 @@
 //alert("JavaScript works!");
 
+/*// Problem 1
 var email = "TrvisBritt@ymail.com";
 
 var emailValidator = function(email) {
@@ -12,30 +13,34 @@ var emailValidator = function(email) {
 	if (atPosition > 2) {
 
 		console.log("The symbole placed in this location is correct!");
-		
-	}else{
-		
+
+	} else {
+
 		console.log("Error please try again.");
 	}
 
 	if (atPosition < afterAtPosition) {
 
-		console.log("There are to many @ symboles in this email, please try again.");	
-		
-	}
-	
-	if (spaceCheck > 0) {
-		
-		console.log("Please remove all the spaces in your email.");
-		
+		console.log("There are to many @ symboles in this email, please try again.");
+
 	}
 
-	if(periodPosition > 1)
-	
-		console.log("There is a period in the wrong position please try again.")
-//return returnSymbole;
+	if (spaceCheck > 0) {
+
+		console.log("Please remove all the spaces in your email.");
+
+	}
+
+	if (periodPosition < 1) {
+
+		console.log("There is a period in the wrong position please try again.");
+
+	}
+
+	//return returnSymbole;
 
 };
 
 var charReturn = emailValidator(email);
 //console.log("The symbole is " + charReturn);
+*/
